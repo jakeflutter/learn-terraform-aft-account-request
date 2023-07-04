7e77cceb5848cb547d90b467b4a93d03f60191c1
jakeflutter <136488881+jakeflutter@users.noreply.github.com>
2023-07-04 11:14:51 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "jakesky@amazon.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
+    ManagedOrganizationalUnit = "Governance"
     SSOUserEmail              = "jakesky@amazon.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
